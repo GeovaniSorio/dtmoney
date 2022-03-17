483c0b4fb4edd518dbfe2eae5bc9ed811e45709f
Geovani Moraes <geovani.moraes@cadmus.com.br>
2022-03-16 22:35:34 -0300

    Commit 16-03

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
 import {api} from '../../services/api'
 
+interface Transaction{
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 export function TransactionsTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([])
 
     useEffect( () => {
        api.get('/transactions')
-       .then(response => console.log(response.data))
+       .then(response => setTransactions(response.data.transactions))
     }, []);
 
     return(
@@ -22,41 +31,24 @@ export function TransactionsTable(){
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
+                  {transactions.map(transaction => (
+                    <tr key={transaction.id}>
+                      <td>{transaction.title}</td>
+                      <td className={transaction.type}>
+                          {`${transaction.type === 'withdraw' ? '-': ''}`}
+                          {new Intl.NumberFormat('pt-BR',{
+                              style: 'currency',
+                              currency: 'BRL'
+                          }).format(transaction.amount)}
+                      </td>
+                      <td>{transaction.category}</td>
+                      <td>
+                          {new Intl.DateTimeFormat('pt-BR').format(
+                              new Date(transaction.createdAt)
+                           )}
+                      </td>
                     </tr>
-
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">-R$1.100</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>25/03/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Gasto com Website</td>
-                        <td className="withdraw">-R$1.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>10/02/2021</td>
-                    </tr>
-
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td className="deposit">R$2.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/05/2021</td>
-                    </tr>
-
+                  ))}
                 </tbody>
             </table>
         </Container>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://10.254.13.77:3000/api'
+    baseURL: 'http://localhost:3000/api'
 })
